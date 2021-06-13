266ee7f46c6828aa4127e98589297a74d773639a
sisekoreggie <85833983+sisekotest@users.noreply.github.com>
2021-06-13 18:27:21 +0200

    Add logic for mergeSortedArray

--- src/merge-sorted-array.ts
+++ src/merge-sorted-array.ts
@@ -1,2 +1,22 @@
-export const mergeSortedArray = (nums1, m, nums2, n) => {
-}
+export const mergeSortedArray = (nums1: Array<number>, m: number, nums2: Array<number>, n: number): Array<number> => {
+    if (n === 0) return nums1;
+
+    let firstElementInNums2 = nums2.shift();
+
+    nums1.every((element, index) => {
+        if (element > firstElementInNums2) {
+            nums1.splice(index, 0, firstElementInNums2);
+            nums1.splice(-1, 1);
+            firstElementInNums2 = nums2.shift();
+        }
+
+        if (element === 0 && index > 0) {
+            nums1.splice(index, 1, firstElementInNums2);
+            firstElementInNums2 = nums2.shift();
+        }
+
+        return !!firstElementInNums2;
+    });
+
+    return nums1;
+};
