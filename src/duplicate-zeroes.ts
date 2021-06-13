b31d0cbedb4353968577df1d70693099ee74ce8f
sisekoreggie <85833983+sisekotest@users.noreply.github.com>
2021-06-13 16:42:48 +0200

    Add duplicateZeroes logic and fix unit tests

--- src/duplicate-zeroes.ts
+++ src/duplicate-zeroes.ts
@@ -1,2 +1,11 @@
-export const duplicateZeroes = (nums) => {
-}
+export const duplicateZeroes = (array: Array<number>): void => {
+    if (!array?.length) return;
+
+    const tempArray = [...array];
+    tempArray.forEach((element, index) => {
+        if (element === 0) {
+            array.splice(index + 1, 0, 0);
+            array.splice(-1, 1);
+        }
+    });
+};
--- src/is-mountain-array.test.ts
+++ src/is-mountain-array.test.ts
@@ -3,19 +3,19 @@ import { isMountainArr } from './is-mountain-array';
 describe('is mountain array tests', () => {
     test('[3, 5, 5] returns false', () => {
         const nums = [3, 5, 5];
-        exptected = false;
+        const expected = false;
         expect(isMountainArr(nums)).toBe(expected);
     });
 
     test('[0, 3, 2, 1] returns true', () => {
         const nums = [0, 3, 2, 1];
-        exptected = true;
+        const expected = true;
         expect(isMountainArr(nums)).toBe(expected);
     });
 
     test('[0, 2, 3, 3, 5, 2, 1, 0] returns false', () => {
         const nums = [0, 2, 3, 3, 5, 2, 1, 0];
-        exptected = false;
+        const expected = false;
         expect(isMountainArr(nums)).toBe(expected);
     });
 });
